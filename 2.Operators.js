e7049c19dd6feb2b17b4998acdab6526548e3505
mehmetipek <66027285+mehmetipek0@users.noreply.github.com>
2022-07-11 00:07:20 +0300

    Add files via upload

---
+++ 2.Operators.js
@@ -0,0 +1,21 @@
+// Mathematical Operators
+
+var x=10;
+var y=20;
+var z=10;
+
+console.log(x+y);
+console.log(x-y);
+console.log(y/x);
+console.log(x*(y+z));
+x++;
+console.log(x);
+
+// Comparing Operators
+
+var answer = x>y;
+console.log(answer); //boolean değer döndürüyor
+console.log(x=='100');
+
+//Boolean Operators
+//   || ve &&  // NOT operatörü !
